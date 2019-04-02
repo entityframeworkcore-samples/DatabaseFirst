12eb1efdce7c53242e5a9cf34037561defaff90e
Jesus Estevez <jecaestevez@outlook.com>
2019-04-02 23:32:11 +0200

    Step 12 Update Database Context

--- DAL/EfDbContext.cs
+++ DAL/EfDbContext.cs
@@ -6,11 +6,23 @@ namespace DAL.JecaestevezApp
 {
     public class EfDbContext : DbContext
     {
-        DbSet<Item> Items { get; set; }
+        public DbSet<Item> Items { get; set; }
+
+        public EfDbContext()
+        {
+
+        }
+        public EfDbContext(DbContextOptions<EfDbContext> options) : base(options)
+        {
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //TODO Extract connection string to a secret
-            optionsBuilder.UseSqlServer(@"Server=.\;Database=EFDatabaseFirstDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+            if (!optionsBuilder.IsConfigured)
+            {
+                //TODO Extract connection string to a secret
+                optionsBuilder.UseSqlServer(@"Server=.\;Database=EFDatabaseFirstDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
         }
     }
 }
